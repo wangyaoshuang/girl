f572776fe97bd2b84d9a0976a994d42ec597d560
hfq-wangyaoshuang <wangyaoshuang@huizhaofang.com>
2019-05-14 15:15:00 +0800

    test

--- src/main/java/com/imooc/gril/GrilApplication.java
+++ src/main/java/com/imooc/gril/GrilApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GrilApplication {
 
     public static void main(String[] args) {
+        System.out.println("testHello");
         SpringApplication.run(GrilApplication.class, args);
     }
 }
